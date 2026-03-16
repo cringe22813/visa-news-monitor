12e9c4caf191103b86912e48c4aadb1afab420aa
cringe22813 <166897970+cringe22813@users.noreply.github.com>
2026-03-16 22:32:12 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -11,14 +11,25 @@ HASH_FILE = "last_hash.txt"
 
 
 def get_latest_news():
+
     params = {
         "country": "by",
         "vac": "byMSQ2it",
         "locale": "ru-ru"
     }
 
-    r = requests.get(URL, params=params, timeout=30)
-    r.raise_for_status()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Referer": "https://visas-it.tlscontact.com/ru-ru/country/by/vac/byMSQ2it/news",
+        "Origin": "https://visas-it.tlscontact.com"
+    }
+
+    r = requests.get(URL, params=params, headers=headers, timeout=30)
+
+    if r.status_code != 200:
+        print("Ошибка API:", r.status_code)
+        return None
 
     data = r.json()
 
@@ -35,6 +46,7 @@ def get_latest_news():
 
 
 def send_telegram(message):
+
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         data={
@@ -46,9 +58,11 @@ def send_telegram(message):
 
 
 def main():
+
     news = get_latest_news()
 
     if not news:
+        print("Не удалось получить новости")
         return
 
     current_hash = hashlib.md5(news.encode()).hexdigest()
